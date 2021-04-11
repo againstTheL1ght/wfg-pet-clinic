b4ee9875700900ca8b684873a1009de6a558ec31
jabiog <john.abiog@webbfontaine.com>
2021-04-11 19:52:28 +0800

    Initial commit

---
+++ src/main/java/com/webbfontaine/wfgpetclinic/WfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.webbfontaine.wfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WfgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webbfontaine/wfgpetclinic/WfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbfontaine.wfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
